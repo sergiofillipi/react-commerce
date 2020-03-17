adddc0c0e8b9a7a56691b4ea7e3ce5a39e3f3174
Sergio Fillipi <sergio.fillipi19@gmail.com>
2020-03-17 17:05:17 -0300

    adding hompage category menu

---
+++ src/pages/homepage.page.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () =>{
+    return (
+        <div className='homepage'>
+            <div className='homepage__category-menu'>
+                <div className='homepage__menu-item'>
+                    <div className="homepage__menu-item-content">
+                        <h1 className="homepage__menu-item-title">
+                            HATS
+                        </h1>
+                        <span className="homepage__menu-item-call-to-action">
+                            Compre agora
+                        </span>
+                    </div>
+                </div>
+                <div className='homepage__menu-item'>
+                    <div className="homepage__menu-item-content">
+                        <h1 className="homepage__menu-item-title">
+                            HATS
+                        </h1>
+                        <span className="homepage__menu-item-call-to-action">
+                            Compre agora
+                        </span>
+                    </div>
+                </div>
+                <div className='homepage__menu-item'>
+                    <div className="homepage__menu-item-content">
+                        <h1 className="homepage__menu-item-title">
+                            HATS
+                        </h1>
+                        <span className="homepage__menu-item-call-to-action">
+                            Compre agora
+                        </span>
+                    </div>
+                </div>
+                <div className='homepage__menu-item'>
+                    <div className="homepage__menu-item-content">
+                        <h1 className="homepage__menu-item-title">
+                            HATS
+                        </h1>
+                        <span className="homepage__menu-item-call-to-action">
+                            Compre agora
+                        </span>
+                    </div>
+                </div>
+                <div className='homepage__menu-item'>
+                    <div className="homepage__menu-item-content">
+                        <h1 className="homepage__menu-item-title">
+                            HATS
+                        </h1>
+                        <span className="homepage__menu-item-call-to-action">
+                            Compre agora
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
