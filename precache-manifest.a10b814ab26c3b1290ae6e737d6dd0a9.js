3faab94a57e648ec048e2d68e044f47feb15ccc9
Sergio Fillipi <sergio.fillipi19@gmail.com>
2020-03-20 20:28:22 -0300

    Updates

---
+++ precache-manifest.a10b814ab26c3b1290ae6e737d6dd0a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168e79f1ea4c02feb5735324905103cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c58cce3d35ceb6f8b8",
+    "url": "/static/css/main.8eb3b9cf.chunk.css"
+  },
+  {
+    "revision": "942ec4f498ed208c36a6",
+    "url": "/static/js/2.88cc2d08.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.88cc2d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c58cce3d35ceb6f8b8",
+    "url": "/static/js/main.547fae5b.chunk.js"
+  },
+  {
+    "revision": "e893c443b0d75034afed",
+    "url": "/static/js/runtime-main.37e10bcf.js"
+  }
+]);
